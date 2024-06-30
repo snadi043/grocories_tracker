4a11d1d0fc59d91e83038fbfdfc7107c42d8678e
snadi043 <snadi043@uottawa.ca>
2024-06-30 13:29:23 +0530

    Created models and data files to manage the application using both named and position properties.

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,21 @@
+import 'package:grocories_tracker/models/grocery_model.dart';
+import 'package:grocories_tracker/data/categories.dart';
+import 'package:grocories_tracker/models/category_model.dart';
+
+final groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
