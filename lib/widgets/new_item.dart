7a95fac7f99deef985f56e74a2bef19aaf67e051
snadi043 <snadi043@uottawa.ca>
2024-07-01 18:44:54 +0530

    Added form inputs to enter the quantity and select the categery of the quantity. Also, placed buttons to reset and add items to the list.

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocories_tracker/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -13,13 +14,65 @@ class _NewItem extends State<NewItem> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add New Item'),
-      ),
-      body: const Padding(
-        padding: const EdgeInsets.all(12),
-        child: Text('The form'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Add New Item'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  maxLength: 50,
+                  decoration: const InputDecoration(label: Text('Name')),
+                  validator: (value) => 'Validation',
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: '1',
+                        decoration: const InputDecoration(
+                          label: Text('Quantity'),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: DropdownButtonFormField(items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                    width: 16,
+                                    height: 16,
+                                    color: category.value.color),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ], onChanged: (value) {}),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(onPressed: () {}, child: const Text('Reset')),
+                    ElevatedButton(
+                        onPressed: () {}, child: const Text('Add Item')),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
